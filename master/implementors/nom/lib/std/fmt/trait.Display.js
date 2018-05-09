a117583dea2744dea4b00955b843404654b2880f
Travis CI User <travis@example.org>
2018-05-09 11:23:59 +0000

    Automatic Travis documentation build

--- master/implementors/nom/lib/std/fmt/trait.Display.js
+++ master/implementors/nom/lib/std/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["nom"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"nom/lib/std/fmt/trait.Display.html\" title=\"trait nom::lib::std::fmt::Display\">Display</a> for <a class=\"struct\" href=\"nom/types/struct.CompleteStr.html\" title=\"struct nom::types::CompleteStr\">CompleteStr</a>&lt;'a&gt;",];
+implementors["nom"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"nom/lib/std/fmt/trait.Display.html\" title=\"trait nom::lib::std::fmt::Display\">Display</a> for <a class=\"struct\" href=\"nom/types/struct.CompleteStr.html\" title=\"struct nom::types::CompleteStr\">CompleteStr</a>&lt;'a&gt;",synthetic:false,types:["nom::types::CompleteStr"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
